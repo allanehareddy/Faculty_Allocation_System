ade96b4bfc948fe94c590dda6fdae8de6250663b
allanehareddy <nehareddy.alla17@gmail.com>
2025-10-08 14:28:18 +0530

    Added backend Spring Boot project

---
+++ demo/src/test/java/com/Faculty_Allocation_System/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Faculty_Allocation_System.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
